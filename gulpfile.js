9f3b2ccce4229f577ea74de0cfe1c022aeacd029
VIETTELGROUP\Phucnh36 <phucnh36@viettel.com.vn>
2023-03-23 13:34:16 +0700

    Backup

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@ const gulpClean = require('gulp-clean');
 const gulpReplace = require('gulp-replace');
 const gulpFilter = require('gulp-filter');
 const gulpRename = require('gulp-rename');
+const gulpTap = require('gulp-tap');
 const jsonEdit = require('gulp-json-editor')
 const less = require('less');
 const LessPluginCleanCSS = require('less-plugin-clean-css');
 const fs = require('fs-extra');
 const path = require('path')
+const _ = require('lodash')
 
 const currentDir = __dirname
 const outDir = path.join(currentDir, 'dist')
@@ -17,7 +19,6 @@ const themePath = "./src/ui-vts.less"
 const themeContent = `
 @import '${themePath}';
 `;
-
 
 function compileLess(cb) {
     less.render(themeContent, {
@@ -65,8 +66,52 @@ function extract() {
         .pipe(dest('extracted'))
 }
 
+function cleanGenerate() {
+    return src('src/*.less', {base: './'})
+        .pipe(gulpClean(null))
+}
+
+function generateTheme() {
+    const themeTemplate = `
+        @import './patch/index';
+        @import './style/core/index';
+        @import './variables-<%= theme %>';
+        @import './components/index';
+    `
+    const variableTemplate = `
+        @import "./style/color/colors";
+        @import "./style/themes/<%= theme %>";
+    `
+
+    return src('src/style/themes/*.less', {base: './'})
+        .pipe(gulpTap((file) => {
+            const withoutExt = file.basename.replace(file.extname, '')
+            const theme = withoutExt
+            const themeContent = _.template(themeTemplate)({theme})
+            const themeFile = `${withoutExt}.less`
+            const variableContent = _.template(variableTemplate)({theme})
+            const variableFile = `variables-${withoutExt}.less`
+            fs.writeFileSync(path.join('src', themeFile), themeContent.replace(/ /g, '').trim())
+            fs.writeFileSync(path.join('src', variableFile), variableContent.replace(/ /g, '').trim())
+        }))
+}
+
+function generateDefault(d) {
+    const defaultContent = `@import "./default";`
+    const variableContent = `@import "./variables-default";`
+    fs.writeFileSync(path.join('src', 'ui-vts.less'), defaultContent)
+    fs.writeFileSync(path.join('src', 'variables.less'), variableContent)
+    d()
+}
+
 task('extract', series(
     extract
+))
+
+task('generate', series(
+    cleanGenerate,
+    generateTheme,
+    generateDefault
 ))
 
 task('build', series(
